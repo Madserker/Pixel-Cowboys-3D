753f1ff30504e2e6c078fd72519d8205aecb3d09
Sergi Hurtado <madserker@gmail.com>
2019-02-16 19:36:27 +0100

    first commit Networking and movement

---
+++ Assets/scripts/PlayerObject.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour {
+	// Start is called before the first frame update
+	private CharacterController controller;
+	public float speed = 6.0f;
+	public float jumpSpeed = 8.0f;
+	public float gravity = 20.0f;
+
+	private Animator anim;
+
+	private Vector3 moveDirection = Vector3.zero;
+
+
+	public float mouseSensitivity = 100.0f;
+	public float clampAngle = 80.0f;
+
+	private float rotY = 0.0f; // rotation around the up/y axis
+	private float rotX = 0.0f; // rotation around the right/x axis
+
+	void Start()
+	{
+		//Is this my object?
+		if (isLocalPlayer == false) {
+			//this obj belongs to another player
+			return;
+		}
+		//since the playerobject is invisible and not part of the world,
+		//give me something physical to move
+
+		CmdSpawnMyUnit();
+	}
+
+	public GameObject PlayerUnitPrefab;
+
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	GameObject myPlayerUnit;
+	//COMMAND THE SERVER
+	[Command]
+	void CmdSpawnMyUnit() {
+		//We are guarenteed to be on the server
+		GameObject go = Instantiate(PlayerUnitPrefab);
+		myPlayerUnit = go;
+		//go.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient)
+		//propagate to all the clients
+		NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+	}
+	
+}
---
+++ Assets/scripts/isRuning.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class isRuning : NetworkBehaviour {
+	private Animator anim;
+	// Start is called before the first frame update
+
+	void Start()
+    {
+		anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (hasAuthority == false)
+		{
+			return;
+		}
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+		{
+			anim.SetBool("isRuning", true);
+		}
+		else {
+			anim.SetBool("isRuning", false);
+		}
+	}
+}
